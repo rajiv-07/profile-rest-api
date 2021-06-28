61fe2b0584b4b70a78e00c1382b4f573939543d3
Rajiv <rajiv.mandapati@gmail.com>
2021-06-28 16:55:00 +0530

    Added Profiles viewset and also authentications for users to edit profiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 
+
+from profiles_api import models
 from profiles_api import serializers
+from profiles_api import permissions
 
 
 
@@ -95,3 +99,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy (self,request,pk=None):
         """Handle Removing an object"""
         return Response({'http_method':'DeLETE'})
+
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpadateOwnProfile,)
